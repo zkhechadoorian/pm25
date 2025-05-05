71bcc022043a2f5af35b71bf696cc6d0e7c8ff1c
Zepyoor Khechadoorian <zepyoorkhechadoorian@gmail.com>
2025-05-04 21:58:42 -0400

    Debugging streamlit app so that it finds the data file when deployed.

--- streamlit_app/scripts/data_loader.py
+++ streamlit_app/scripts/data_loader.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import streamlit as st
+from pathlib import Path
+import ssl
+import certifi
+import urllib.request
 
 # Using Streamlit's caching to speed up repeated data loading
 @st.cache_data
-def load_data(filepath="../data/processed/pm25_cleaned.csv"):
+
+def load_data(filepath="./data/processed/pm25_cleaned.csv"):
     """
     This function loads the cleaned PM2.5 dataset from the given filepath.
     It also extracts the 'Year' from the 'Period' column and adds it as a new column.
@@ -14,7 +19,19 @@ def load_data(filepath="../data/processed/pm25_cleaned.csv"):
     Returns:
     - df: The cleaned DataFrame with an additional 'Year' column
     """
-    
+
+    # Option 3
+    url="https://raw.githubusercontent.com/zkhechadoorian/pm25/refs/heads/main/data/processed/pm25_cleaned.csv"
+    # Create an SSL context using certifi
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+    # Fetch the CSV file from the URL
+    with urllib.request.urlopen(url, context=ssl_context) as response:
+        df = pd.read_csv(response)
+    return df
+
+    # Option 1
+"""
     # Load the data from the provided CSV file path
     df = pd.read_csv(filepath)
     
@@ -23,3 +40,12 @@ def load_data(filepath="../data/processed/pm25_cleaned.csv"):
     
     # Return the dataframe with the new 'Year' column
     return df
+
+        # Option 2
+    # Get the absolute path relative to the script's location
+    base_path = Path(__file__).parent.parent.parent  # Adjust based on your directory structure
+    absolute_path = base_path / filepath
+    df = pd.read_csv(absolute_path)
+    df['Year'] = pd.to_datetime(df['Period'], format='%Y').dt.year
+    return df
+"""
